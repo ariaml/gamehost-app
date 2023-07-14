96d3afebd4aa4f04c157c3755e7be1fbf3dde27d
ariaml <airamariedecastroladera@gmail.com>
2023-07-14 16:20:02 +0800

    only navbar update

--- src/App.js
+++ src/App.js
@@ -1,68 +1,53 @@
 import React from 'react';
-import { Stack, HStack, Heading, Spacer } from '@chakra-ui/react';
-import { Icon } from '@chakra-ui/icon'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import { GiTreasureMap } from 'react-icons/gi'
-import HomePage from './components/HomePage';
-import StorePage from './components/StorePage';
-import CommunityPage from './components/CommunityPage';
-import SupportPage from './components/SupportPage';
-import SignUpPage from './components/SignUpPage';
-import LogInPage from './components/LogInPage';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { Tab, TabList, Tabs } from '@chakra-ui/react';
+import HomePage from './pages/HomePage';
+import StorePage from './pages/StorePage';
+import CommunityPage from './pages/CommunityPage';
+import SupportPage from './pages/SupportPage';
+import SignUpPage from './pages/SignUpPage';
+import LogInPage from './pages/LogInPage';
+
+function NavBar() {
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>
+          <Link to='/home'>HOME</Link>
+        </Tab>
+        <Tab>
+          <Link to='/store'>STORE</Link>
+        </Tab>
+        <Tab>
+          <Link to='/community'>COMMUNITY</Link>
+        </Tab>
+        <Tab>
+          <Link to='/support'>SUPPORT</Link>
+        </Tab>
+        <Tab align='end'>
+          <Link to='signup'>SIGN UP</Link>
+        </Tab>
+      </TabList>
+    </Tabs>
+  );
+}
 
 function App() {
   return (
-    <Stack bg='#212121'>
-      <HStack mt='1' w='100%'>
-        {/* <Icon ml='50' as={GiTreasureMap} boxSize={7}/> */}
-        <Tabs ml='5'>
-          <TabList>
-            <Tab>HOME</Tab>
-            <Tab>STORE</Tab>
-            <Tab>COMMUNITY</Tab>
-            <Tab>SUPPORT</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <HomePage />
-            </TabPanel>
-            <TabPanel>
-              <StorePage/>
-            </TabPanel>
-            <TabPanel>
-              <CommunityPage/>
-            </TabPanel>
-            <TabPanel>
-              <SupportPage/>
-            </TabPanel>
-          </TabPanels>
-
-        </Tabs>
-        <Spacer></Spacer>
-        <Tabs align='end'>
-          <TabList>
-            <Tab>SIGN UP</Tab>
-            <Tab>LOG IN</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <SignUpPage/>
-            </TabPanel>
-            <TabPanel>
-              <LogInPage/>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </HStack>
-      
-      {/* <HomePage></HomePage>
-      <StorePage></StorePage>
-      <CommunityPage></CommunityPage>
-      <SupportPage></SupportPage>
-      <SignUpPage></SignUpPage>
-      <LogInPage></LogInPage> */}
-
-    </Stack>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/store' element={<StorePage />} />
+          <Route path='/community' element={<CommunityPage />} />
+          <Route path='/support' element={<SupportPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/login' element={<LogInPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
